4c1299e420766ad4eda665ce8c6d3dca0806dae0
John Coughlin <coughlinj123@gmail.com>
2021-02-04 17:35:51 -0600

    Main Code for Project

---
+++ FinalPiProject.py
@@ -0,0 +1,72 @@
+# Names: John Coughlin, Seth Flowers
+
+# Final Pi Project
+
+# Description:
+
+
+from tkinter import *
+
+class Story:
+    #Class variables
+
+    
+    def __init__(self):
+        #Instance variables
+
+    def addEvent():
+        pass
+
+    def addArea():
+        pass
+
+    def addSideQuest():
+        pass
+
+class Events:
+    def __init__(self):
+        
+# Setup method
+
+def Setup():
+    #Main = Story()
+
+    #Events:
+    
+    #Main.addEvent()
+
+
+    #Story Areas:
+
+    #Main.addArea()
+
+
+    #Chase scene:
+
+
+
+    #Timed Events:
+
+
+
+    #Tkinter:
+
+
+
+    #Side Quests:
+
+    #Main.addSideQuest()
+
+    
+    pass
+
+def StoryLoop():
+    pass
+
+
+
+
+#Main part of the code:
+
+Setup()
+StoryLoop()
